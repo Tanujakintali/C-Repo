218543b28a9386385bc6bdf339caf8ac796aa540
Tanujakintali <tanujakinthali@gmail.com>
2025-01-01 21:16:11 +0530

    Uploading code: Sum of Even Rows and Sum of Odd Rows in a Matrix.c

--- Sum of Even Rows and Sum of Odd Rows in a Matrix.c
+++ Sum of Even Rows and Sum of Odd Rows in a Matrix.c
@@ -1,18 +1,17 @@
 #include<stdio.h>
 int main()
 {
-    int x,y;
-    scanf("%d%d",&x,&y);
-    int i,j;
-    int a[x][y];
-    for(i=0;i<x;i++){
-        for(j=0;j<y;j++){
+    int n,m;
+    scanf("%d%d",&n,&m);
+    int a[n][m],i,j;
+    for(i=0;i<n;i++){
+        for(j=0;j<m;j++){
             scanf("%d",&a[i][j]);
         }
     }
     int es=0,os=0;
-    for(i=0;i<x;i++){
-        for(j=0;j<y;j++){
+    for(i=0;i<n;i++){
+        for(j=0;j<m;j++){
             if(i%2==0){
                 es+=a[i][j];
             }else{
